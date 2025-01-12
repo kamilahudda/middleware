0bd2de723029374d3b9ad4b74cf8469ff9c2e165
kamilahudda <112380165+kamilahudda@users.noreply.github.com>
2025-01-12 21:41:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const userRoute = require('./routes/user-route');
-const logger = require('./middlewares/user-logger');
+const express = require("express");
+const userRoute = require("./routes/user-route");
+const userLogger = require("./middlewares/user-logger");
 
 const app = express();
-
+app.use(userLogger);
 app.use(express.json());
-app.use('/api/users', userRoute);
-app.use(logger)
+app.use("/api/users", userRoute);
 
-const port = 3000;
-
+const port = 2000;
 app.listen(port, () => {
-    console.log(`Server run at http://localhost:${port}/api/users`);
+  console.log(`Server running at http://localhost:${port}/api/users`);
 });
