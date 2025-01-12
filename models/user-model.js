05dfecac44b72d43e9d1a4fe0bf668fb7a7ecb18
kamilahudda <112380165+kamilahudda@users.noreply.github.com>
2025-01-12 21:38:19 +0700

    Update user-model.js

--- models/user-model.js
+++ models/user-model.js
@@ -1,14 +1,22 @@
-const dataUser = ['Andi', 'Bob', 'Ciro']
+const dataUser = ["Andi", "Bob", "Ciro"];
 
 const getUser = () => {
-    return dataUser
-}
+  return dataUser;
+};
 
 const getUserByIndex = (id) => {
-    if (id <= dataUser.length) {
-        return dataUser[id - 1]
-    }
-    return "Index is greater than Array Length"
-}
+  if (id > 0 && id <= dataUser.length) {
+    return dataUser[id - 1];
+  }
+  return "Index is greater than Array Length";
+};
 
-module.exports = { getUser, getUserByIndex }
+const deleteUser = (id) => {
+  if (id >= 0 && id < dataUser.length) {
+    dataUser.splice(id, 1); // Menghapus user berdasarkan index
+    return dataUser; // Mengembalikan array user terbaru
+  }
+  return "Invalid Index";
+};
+
+module.exports = { getUser, getUserByIndex, deleteUser };
