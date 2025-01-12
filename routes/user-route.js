bdc69080d1df1e7d22b4737cac2f4519f3aa2e6e
kamilahudda <112380165+kamilahudda@users.noreply.github.com>
2025-01-12 21:41:13 +0700

    Update user-route.js

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,10 +1,11 @@
-const userController = require('../controllers/user-controller')
-const userAuth = require('../middlewares/user-auth')
-const express = require('express')
+const userController = require("../controllers/user-controller");
+const userAuth = require("../middlewares/user-auth");
 
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', userController.getUsers)
-router.get('/:id', userAuth, userController.getUserByIndex)
+router.get("/", userController.getUser);
+router.get("/:id", userAuth, userController.getUserByIndex);
+router.delete("/:id", userAuth, userController.deleteUserByIndex);
 
-module.exports = router
+module.exports = router;
